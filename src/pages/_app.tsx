56c49b650e9236d25c8ec11982e9e74994fab334
Dimon <mamatusupovdima@gmail.com>
2020-11-01 14:11:09 +0200

    add icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,23 +5,20 @@ import React from "react";
 import {AppProps} from "next/app";
 import Head from "next/head";
 import {cartReducer} from "../store";
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import {ThemeProvider} from "@material-ui/styles";
 import theme from "../theme";
 
 function MyApp({Component, pageProps}: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>Я поглажу?</title>
-                <meta name="theme-color" content="#FFFFFF"/>
-                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-            </Head>
-            <ThemeProvider theme={theme}>
-                <Provider store={cartReducer}>
-                    <Component {...pageProps} />
-                </Provider>
-            </ThemeProvider>
+    return (<>
+        <Head>
+            <title>Погладить можно?</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+            <Provider store={cartReducer}>
+                <Component {...pageProps} />
+            </Provider>
+        </ThemeProvider>
         </>
     )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,13 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
                     {/* PWA primary color */}
                     <meta name="theme-color" content={theme.palette.primary.main}/>
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
                 </Head>
                 <body>
                 <Main/>
