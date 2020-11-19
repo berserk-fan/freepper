b8acc47825b05d067aea5acba0728db5e2bca16f
mamat14 <60214862+mamat14@users.noreply.github.com>
2020-11-19 22:26:51 +0200

    side bar (#13)

--- src/components/Layout/Header/HeaderMenu.tsx
+++ src/components/Layout/Header/HeaderMenu.tsx
@@ -1,13 +1,14 @@
-import { Box, Drawer, IconButton } from "@material-ui/core";
+import {Box, Drawer, Fade, fade, IconButton, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import theme from "../../../theme";
 import Link from "next/link";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
+import {useRouter} from "next/router";
+import HomeIcon from '@material-ui/icons/Home';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles({
   list: {
@@ -21,11 +22,21 @@ const useStyles = makeStyles({
   },
   drawer: {
     width: "70vw",
+    maxWidth: "300px",
+    overflow: "visible"
   },
+  closeMenuButton: {
+    position: "absolute",
+    top: 5,
+    right: -65,
+    background: theme.palette.background.paper,
+    borderRadius: '50%'
+  }
 });
 
 export default function HeaderMenu() {
   const classes = useStyles();
+  const router = useRouter();
 
   const [drawerOpen, setDrawerTo] = React.useState(false);
 
@@ -40,6 +51,13 @@ export default function HeaderMenu() {
     setDrawerTo(open);
   };
 
+  const pages: [string, string, React.ReactNode][] = [
+    ["/", "Домой", <HomeIcon/>],
+    ["/shop", "Магазин", <StorefrontIcon/>],
+    ["/about", "О наc", <InfoOutlinedIcon/>],
+  ];
+  const sideBarOpenTime = 250;
+
   return (
     <>
       <IconButton
@@ -52,28 +70,40 @@ export default function HeaderMenu() {
         <MenuIcon />
       </IconButton>
       <Drawer
+        className={"relative"}
         classes={{ paper: classes.drawer }}
         open={drawerOpen}
         onClose={toggleDrawer(false)}
+        transitionDuration={sideBarOpenTime}
       >
-        <ButtonGroup
-          orientation={"vertical"}
-          className={``}
-          color="primary"
-          aria-label="page tabs"
+        <Fade in={drawerOpen} style={{transitionDelay: drawerOpen ? `${sideBarOpenTime / 2}ms` : `0ms`}}>
+          <Box component={"span"} className={classes.closeMenuButton} onClick={toggleDrawer(false)}>
+            <IconButton>
+              <CloseIcon fontSize={"large"}/>
+            </IconButton>
+          </Box>
+        </Fade>
+        <List component="nav"
+            aria-label="home shop about"
         >
-          {[
-            ["/", "Домой"],
-            ["/shop", "Магазин"],
-            ["/about", "О наc"],
-          ].map(([path, name]) => {
+          {pages.map(([path, name, icon]) => {
             return (
-              <Link key={name + path} href={path}>
-                <Button className={classes.menuButton}>{name}</Button>
-              </Link>
+                <Link key={name + path} href={path}>
+                  <ListItem
+                      button
+                      selected={router.pathname === path}
+                  >
+                        <ListItemIcon>
+                          {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={name}>
+                          {name}
+                        </ListItemText>
+                  </ListItem>
+                </Link>
             );
           })}
-        </ButtonGroup>
+        </List>
       </Drawer>
     </>
   );
