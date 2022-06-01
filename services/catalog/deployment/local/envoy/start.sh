47a96d27bd452ee3ab65ad394b83723133c549d3
John Doe <mypassion@i.ua>
2022-06-01 20:07:13 +0200

    add envoy grpc port

--- services/catalog/deployment/local/envoy/start.sh
+++ services/catalog/deployment/local/envoy/start.sh
@@ -3,9 +3,9 @@ set -e -x
 
 DIR_NAME="$(dirname "$0")"
 (
-  export $(grep -v '^#' .env | xargs);
+  export $(grep -v '^#' .env.local | xargs);
   #populate env substitutions in yaml
   cat "${DIR_NAME/local/common}"/envoy.tmpl.yaml | envsubst > $DIR_NAME/'envoy.yaml'
 )
-docker-compose -f $DIR_NAME/docker-compose.yaml -p catalog down || echo "already down"
-docker-compose -f $DIR_NAME/docker-compose.yaml -p catalog --env-file .env up
+docker-compose -f $DIR_NAME/docker-compose.yaml -p catalog --env-file .env.local up down || echo "already down"
+docker-compose -f $DIR_NAME/docker-compose.yaml -p catalog --env-file .env.local up
