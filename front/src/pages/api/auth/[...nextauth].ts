5e82c4f20794b10dfa11f0e063cc63f4b3b2a4ab
John Doe <mypassion@i.ua>
2023-02-09 11:07:52 +0100

    optional github provider

--- front/src/pages/api/auth/[...nextauth].ts
+++ front/src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,17 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+const githubProvider = process.env.GITHUB_CLIENT_ID
+  ? [
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+    ]
+  : [];
 export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+  providers: githubProvider,
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
 };
